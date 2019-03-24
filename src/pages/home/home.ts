9f6709085290946139d0678da5fde898f1d325db
Kiran Yadubanshi <kiran.yadubanshi@selu.edu>
2019-03-24 14:38:44 -0500

    Functinality added for TipsPage
    Extra: CalculatorPage UI tweak

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController, NavController, ToastController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  billAmount: number;
+  happiness: number;
+  servicerate: any = 'nice';
+  tipPercent: number = 15;
+  numOfPerson: number = 1;
+  tipAmount: number;
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController,
+              public toastCtrl: ToastController) {
 
   }
 
+  GetTipAmount(){
+    if(this.billAmount >= 0) {
+      this.alertCtrl.create({
+        title: 'Tip',
+        message: 'This is the tip amount' + this.calculateTip(this.billAmount,this.tipPercent),
+        buttons: ['Ok']
+      }).present();
+    } else {
+      this.toastCtrl.create({
+        message: 'Enter the bill amount please.',
+        duration: 2000,
+        cssClass: 'amountError'
+      }).present();
+    }
+    if(this.servicerate != undefined){
+      console.log(this.servicerate);
+    }
+    console.log(this.tipPercent);
+    console.log(this.numOfPerson);
+  }
+
+  calculateTip(amt,percent){
+    return this.billAmount*(this.tipPercent/100);
+  }
+
+  HappinessRange($event) {
+    this.happiness = $event;
+    console.log(this.happiness);
+    if (this.happiness < 20) {
+      console.log('The food was bad');
+    }
+  }
 }
